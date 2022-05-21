08685e5d9339267a2e2c097da21da313d081e05c
Krtucho <krtucho98@gmail.com>
2022-05-21 18:49:49 -0400

    player

--- player.pl
+++ player.pl
@@ -39,13 +39,29 @@ concatList([], Z, Z).
 concatList([A|X], Y, [A|Z]) :- 
     concatList(X, Y, Z).
 
+cell_values([], _, []).
+cell_values([[R, C]| T], M, [V|VT]) :-
+    nth0(R, M, Row),
+    nth0(C, Row, V),
+    cell_values(T, M, VT).
+
+update_mat_rc(Mc,R,C,V,Mu) :-
+    nth0(R,Mc,Rc,Mt),
+    nth0(C,Rc,_,Rt),
+    nth0(C,Ru,V,Rt),
+    nth0(R,Mu,Ru,Mt).
+
 
 kk:-
     create_players(2),
    % players(X,Y,(Z,W), R1,R2,R3,R4, W, B),
     %format("Fin del Turno. ~a ~a ~a ~a ~n",[X,Y,Z,W]),
-    concatList([[0,1],[2,3]], [2], V),
-    print(V).
+    %cell_values([[0,0], [0,1], [1,0], [1,1]], [[6,4],[2,3]], V),
+    %print(V).
 
+    
+    update_mat_rc([[1,2,3],[4,5,6],[7,8,9]],1,1,27,Z),
+    %M = [[1, 2, 3], [4, x, 6], [7, 8, 9]].
+    print(Z).
 
 kk.
