4777b45cb7fccc6d6e3dcd43939e1dd27bec18e3
Krtucho <krtucho98@gmail.com>
2022-05-21 20:24:32 -0400

    player and wall

--- player.pl
+++ player.pl
@@ -1,8 +1,16 @@
-:- dynamic players/9.
+:-[wall].
 
 % Predicado dinamico players con 9 argumentos, el 1ro sera el numero del jugador y el 2do la puntuacion del mismo
 % N -> Numero del jugador
 % S -> Puntuacion del jugador
+% R1 -> Piso 1 de la escalera          _
+% R2 -> Piso 2 de la escalera        _ _
+% R3 -> Piso 3 de la escalera      _ _ _
+% R4 -> Piso 4 de la escalera    _ _ _ _
+% R5 -> Piso 5 de la escalera  _ _ _ _ _
+% M -> Mosaico o Matriz de la derecha
+% D -> Fichas descartadas(Dropped Tiles)
+:- dynamic players/9.
 
 % Agrega la clausula a player de un nuevo jugador numero N y con puntuacion=0
 create_player(N):-
@@ -26,30 +34,65 @@ remove_players():-
 % Score
 % Asigna Una puntuacion a un jugador con indice Player
 set_score(Player, Score):-
-    retract(players(Player, _, R1, R2,R3,R4,R5, W, B)),
-    assert(players(Player, Score, R1, R2,R3,R4,R5, W, B)).
+    retract(players(Player, _, R1, R2,R3,R4,R5, M, D)),
+    assert(players(Player, Score, R1, R2,R3,R4,R5, M, D)).
 
 % Aumenta la puntuacion del jugador con indice Player
 add_score(Player, Score_to_Add):-
-    retract(players(Player, Score,R1, R2,R3,R4,R5, W, B)),
+    retract(players(Player, Score,R1, R2,R3,R4,R5, M, D)),
     S is Score+Score_to_Add,
-    assert(players(Player, S, R1, R2,R3,R4,R5, W, B)).
+    assert(players(Player, S, R1, R2,R3,R4,R5, M, D)).
 
-concatList([], Z, Z).
-concatList([A|X], Y, [A|Z]) :- 
-    concatList(X, Y, Z).
+% Actualizar Piso 1 de la escalera
+update_R1(P, R1):-
+    retract(players(Player, S, _, R2,R3,R4,R5, M, D)),
+    assert(players(Player, S, R1, R2,R3,R4,R5, M, D)).
 
-cell_values([], _, []).
-cell_values([[R, C]| T], M, [V|VT]) :-
-    nth0(R, M, Row),
-    nth0(C, Row, V),
-    cell_values(T, M, VT).
+% Actualizar Piso 2 de la escalera
+update_R2(P, R2):-
+    retract(players(Player, S, R1, _,R3,R4,R5, M, D)),
+    assert(players(Player, S, R1, R2,R3,R4,R5, M, D)).
 
-update_mat_rc(Mc,R,C,V,Mu) :-
-    nth0(R,Mc,Rc,Mt),
-    nth0(C,Rc,_,Rt),
-    nth0(C,Ru,V,Rt),
-    nth0(R,Mu,Ru,Mt).
+% Actualizar Piso 3 de la escalera
+update_R3(P, R3):-
+    retract(players(Player, S, R1, R2, _,R4,R5, M, D)),
+    assert(players(Player, S, R1, R2,R3,R4,R5, M, D)).
+
+% Actualizar Piso 4 de la escalera
+update_R4(P, R4):-
+    retract(players(Player, S, R1, R2, R3,R4,R5, M, D)),
+    assert(players(Player, S, R1, R2,R3,R4,R5, M, D)).
+
+% Actualizar Piso 5 de la escalera
+update_R3(P, R5):-
+    retract(players(Player, S, R1, R2, R3,R4,_, M, D)),
+    assert(players(Player, S, R1, R2,R3,R4,R5, M, D)).
+
+
+% Calcular la puntuacion a restar con n fichas descartadas.
+% N numero de fichas descartadas(se incluye la ficha de jugadr inicial)
+% S valor en numeros negativos a descontar o 0 si N es 0
+calculateDropScore(0, 0).
+calculateDropScore(1, -1).
+calculateDropScore(2, -2).
+calculateDropScore(3, -4).
+calculateDropScore(4, -6).
+calculateDropScore(5, -8).
+calculateDropScore(6, -11).
+calculateDropScore(7, -14).
+calculateDropScore(N, -14) :-
+    N > 7.
+
+% Actualizar cantidad de piezas descartadas
+update_dropped_tiles(P, D):-
+    retract(players(P, Score,R1, R2,R3,R4,R5, M, _)),
+    assert(players(P, S, R1, R2,R3,R4,R5, M, D)).
+
+% Actualizar matriz de la derecha(Muro)
+update_matrix(P, M):-
+    retract(players(P, Score,R1, R2,R3,R4,R5, _, D)),
+    assert(players(P, S, R1, R2,R3,R4,R5, M, D)).
+
 
 
 
@@ -62,11 +105,11 @@ kk:-
     %print(V).
 
     
-    update_mat_rc([[1,2,3],[4,5,6],[7,8,9]],1,1,27,Z),
+    %update_mat_rc([[1,2,3],[4,5,6],[7,8,9]],1,1,27,Z),
     %M = [[1, 2, 3], [4, x, 6], [7, 8, 9]].
-    print(Z),
+    %print(Z),
 
-    nth0(2, Z, O),
-    print(O).
+    %nth0(2, Z, O),
+    %print(O).
 
 kk.
---
+++ utils.pl
@@ -0,0 +1,50 @@
+concatList([], Z, Z).
+concatList([A|X], Y, [A|Z]) :- 
+    concatList(X, Y, Z).
+
+%S: 
+%[ 5   6 
+%  3   4 ] 
+% Represented in prolog as: S = [ [5,6], [3,4]].
+
+%I'm trying to write a recursive function to get a cell value such that cell_values (Cells, Matrix, Values) would return a list of the values from a list of the cell.
+%Example: cell_values ([[0,0], [0,1]], S, Values) --> Values = [5, 6]. Where S is the matrix above.
+% R -> 1st value(position x,y) to find
+% C -> 2nd value(position x,y) to find
+% T -> nth value(position x,y) to find
+% M -> Matrix
+% V -> 1st value found
+% VT -> nth value found
+get_values([], _, []).
+get_values([[R, C]| T], M, [V|VT]) :-
+    nth0(R, M, Row),
+    nth0(C, Row, V),
+    get_values(T, M, VT).
+
+%?- update_mat_rc([[1,2,3],[4,5,6],[7,8,9]],1,1,x,M).
+%M = [[1, 2, 3], [4, x, 6], [7, 8, 9]].
+update_mat_rc(Mc,R,C,V,Mu) :-
+    nth0(R,Mc,Rc,Mt),
+    nth0(C,Rc,_,Rt),
+    nth0(C,Ru,V,Rt),
+    nth0(R,Mu,Ru,Mt).
+
+% Index -> Posicion de la lista a buscar
+% Matrix(Matriz o lista en la que vamos a buscar)
+% Fila resultante
+get_row(Index, Matrix, Row):-
+    nth0(Row, Matrix, Row).
+
+%?- get_col( [[1,2], [3, 4], [5,6]], 0, Col).
+%Col = [1, 3, 5].
+
+%?- get_col( [[1,2], [3, 4], [5,6]], I, Col).
+%I = 0,
+%Col = [1, 3, 5] ;
+%I = 1,
+%Col= [2, 4, 6].
+get_col([], _, []).
+get_col([X|Xs], I, [Y|Ys]) :-
+    nth0(I, X, Y),
+    get_col(Xs, I, Ys).
+
